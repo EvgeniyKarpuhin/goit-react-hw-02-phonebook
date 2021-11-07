312f37ae18b1af762a260f8b0e3e2f94b380028b
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-11-08 01:55:06 +0200

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -41,8 +41,11 @@ export default class ContactForm extends Component {
             className={styles.TaskEditor_input}
             type="tel"
             name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             value={this.state.number}
             onChange={this.handleChange}
+            required
           />
         </label>
         <button className={styles.TaskEditor_button} type="submit">
