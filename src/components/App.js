ca74d3cc0103a2fe8bed5ee2c7e9b914e0ca4c25
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-11-08 01:50:46 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,9 @@ export default class App extends Component {
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: "",
+    filter: '',
+    name: '',
+    number: ''
   };
 
   addContact = (task) => {
